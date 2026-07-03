63af401d8792a1c8e7571246d08cfd52fbc45b40
Enita <enitanureya@outlook.com>
2026-07-03 22:48:25 +0800

    Standardize Yggdrasil errors

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::util::SubscriberInitExt;
+
 #[derive(Parser)]
 pub struct Args {
     #[arg(short, long, help = "Enable verbose output")]
@@ -6,4 +9,26 @@ pub struct Args {
 
     #[arg(long, help = "Enable debug output")]
     pub debug: bool,
+}
+
+// Execute argument-specific logics. If the arguments would prevent Aphanite from starting, cli() should std::process::exit on its own.
+pub fn cli(args: &Args) {
+    let filter = std::env::var("RUST_LOG").unwrap_or(format!(
+        "{crate_name}={log_level}",
+        crate_name = env!("CARGO_PKG_NAME"),
+        log_level = if args.debug {
+            "debug"
+        } else if args.verbose {
+            "info"
+        } else {
+            "warn"
+        }
+    ));
+    let env_filter = EnvFilter::new(filter);
+    let subscriber = tracing_subscriber::fmt().with_env_filter(env_filter);
+    if args.debug {
+        subscriber.with_file(true).pretty().finish().init();
+    } else {
+        subscriber.finish().init();
+    }
 }
--- src/yggdrasil/api.rs
+++ src/yggdrasil/api.rs
@@ -2,32 +2,52 @@ use crate::yggdrasil::model::GameProfile;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use serde::{Deserialize, Serialize};
-// Error
 
-pub type Result<T> = std::result::Result<T, Error>;
+pub type Result<T> = std::result::Result<T, YggdrasilError>;
 
+/// Error type defined by the authlib-injector Yggdrasil doc
 #[derive(Debug)]
-pub enum Error {
+pub enum YggdrasilError {
+    /// General HTTP error
     HttpError(StatusCode),
+
+    /// Invalid token or trying to join a server with a wrong profile
     InvalidToken,
+
+    /// Invalid username or password
     InvalidCredentials,
-    IllegalArgumentException,
-    Undefined(String),
+
+    /// Attempt to assign profile to a profile-assigned token
+    IllegalArgument,
+
+    /// Attempt to assign unrelated profile or other general forbidden operations
+    ForbiddenOperation,
+
+    /// General error that is not covered by the doc
+    Other(String),
 }
 
-impl std::fmt::Display for Error {
+/// "Error Message" of the error
+impl std::fmt::Display for YggdrasilError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::HttpError(s) => write!(f, "HTTP error: {}", s),
-            Error::InvalidToken => write!(f, "Invalid token"),
-            Error::InvalidCredentials => write!(f, "Invalid credentials"),
-            Error::IllegalArgumentException => write!(f, "Illegal argument"),
-            Error::Undefined(msg) => write!(f, "Undefined error: {}", msg),
+            YggdrasilError::HttpError(s) => {
+                write!(f, "{}", s.canonical_reason().unwrap_or("Error"))
+            }
+            YggdrasilError::InvalidToken => write!(f, "Invalid token."),
+            YggdrasilError::InvalidCredentials => {
+                write!(f, "Invalid credentials. Invalid username or password.")
+            }
+            YggdrasilError::IllegalArgument => {
+                write!(f, "Access token already has a profile assigned.")
+            }
+            YggdrasilError::ForbiddenOperation => write!(f, "An error has occurred."),
+            YggdrasilError::Other(msg) => write!(f, "An error has occurred: {}", msg),
         }
     }
 }
 
-impl std::error::Error for Error {}
+impl std::error::Error for YggdrasilError {}
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -39,43 +59,48 @@ pub struct ErrorResponse {
 }
 
 impl ErrorResponse {
-    fn new(error: impl Into<String>, message: impl Into<String>) -> Self {
+    fn new(error: &YggdrasilError, error_id: impl Into<String>) -> Self {
         Self {
-            error: error.into(),
-            error_message: message.into(),
+            error: error_id.into(),
+            error_message: format!("{}", error),
             cause: None,
         }
     }
 }
 
-impl IntoResponse for Error {
+impl IntoResponse for YggdrasilError {
     fn into_response(self) -> axum::response::Response {
         let (status, body) = match self {
-            Error::HttpError(s) => (
-                s,
-                ErrorResponse::new(s.canonical_reason().unwrap_or("Error"), ""),
+            YggdrasilError::HttpError(status) => (
+                status,
+                ErrorResponse::new(&self, status.canonical_reason().unwrap_or("Error")),
             ),
 
-            Error::InvalidToken => (
+            YggdrasilError::InvalidToken => (
                 StatusCode::FORBIDDEN,
-                ErrorResponse::new("ForbiddenOperationException", "Invalid token."),
+                ErrorResponse::new(&self, "ForbiddenOperationException"),
             ),
 
-            Error::InvalidCredentials => (
+            YggdrasilError::InvalidCredentials => (
                 StatusCode::FORBIDDEN,
-                ErrorResponse::new("ForbiddenOperationException", "Invalid credentials."),
+                ErrorResponse::new(&self, "ForbiddenOperationException"),
             ),
 
-            Error::IllegalArgumentException => (
+            YggdrasilError::IllegalArgument => (
                 StatusCode::BAD_REQUEST,
-                ErrorResponse::new("IllegalArgumentException", ""),
+                ErrorResponse::new(&self, "IllegalArgumentException"),
             ),
 
-            Error::Undefined(msg) => (
+            YggdrasilError::ForbiddenOperation => (
+                StatusCode::FORBIDDEN,
+                ErrorResponse::new(&self, "ForbiddenOperationException"),
+            ),
+
+            YggdrasilError::Other(msg) => (
                 StatusCode::FORBIDDEN,
                 ErrorResponse {
                     error: "ForbiddenOperationException".into(),
-                    error_message: "".into(),
+                    error_message: format!("An error has occurred: {}", &msg),
                     cause: Some(msg),
                 },
             ),
@@ -170,8 +195,8 @@ struct RequestSignout {
 #[cfg(test)]
 mod test {
     use super::*;
-    use axum::routing::get;
     use axum::Router;
+    use axum::routing::get;
     #[test]
     fn type_test() {
         tokio::runtime::Runtime::new().unwrap().block_on(async {
