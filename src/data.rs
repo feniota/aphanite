c72d0e4489fbbed5b379057fffe9494e55bb6e08
Tuxium <ursusfeline07@gmail.com>
2026-07-05 10:55:22 +0800

    Implement the Yggdrasil API

--- src/data.rs
+++ src/data.rs
@@ -1,6 +1,7 @@
 //! [`DatabaseAccessor`](crate::data::DatabaseAccessor) and everything (except storage) related to database
 
 use crate::types::{Token, User};
+use anyhow::{anyhow, Result};
 use argon2::PasswordVerifier;
 use jiff::ToSpan;
 use toasty::Db;
@@ -19,33 +20,39 @@ impl DatabaseAccessor {
         Self { db }
     }
 
-    pub async fn verify_user(&self, email: &str, password: &str) -> anyhow::Result<bool> {
+    pub async fn verify_user(&self, email: &str, password: &str) -> Result<User> {
         let mut db = self.db.clone();
-        if let Ok(v) = User::get_by_email(&mut db, email).await {
-            let parsed_hash = argon2::PasswordHash::new(&v.password)?;
-            Ok(argon2::Argon2::default()
-                .verify_password(password.as_bytes(), &parsed_hash)
-                .is_ok())
-        } else {
-            Ok(false)
-        }
-    }
-    pub async fn verify_token(&self, access_token: &Uuid, client_token: Option<&str>) -> bool {
-        let mut db = self.db.clone();
-        if let Ok(token) = Token::get_by_access_token(&mut db, access_token).await {
-            if token.created_at + TOKEN_TTL_SECS.seconds() < jiff::Timestamp::now() {
-                if let Err(e) = Token::delete_by_access_token(&mut db, access_token).await {
-                    error!("{e}")
-                }
-                return false;
-            }
+        let user = User::get_by_email(&mut db, email).await?;
+        let parsed_hash = argon2::PasswordHash::new(&user.password)
+            .expect("Database error: Unable to parse the user password");
+        argon2::Argon2::default().verify_password(password.as_bytes(), &parsed_hash)?;
 
-            match client_token {
-                None => true,
-                Some(v) => v == token.client_token,
+        Ok(user)
+    }
+    pub async fn verify_token(
+        &self,
+        access_token: &Uuid,
+        client_token: Option<&str>,
+    ) -> Result<()> {
+        let mut db = self.db.clone();
+        let token = Token::get_by_access_token(&mut db, access_token).await?;
+
+        if token.created_at + TOKEN_TTL_SECS.seconds() < jiff::Timestamp::now() {
+            if let Err(e) = Token::delete_by_access_token(&mut db, access_token).await {
+                error!("{e}")
             }
-        } else {
-            false
+            return Err(anyhow!("The access token has expired."));
+        }
+
+        match client_token {
+            None => Ok(()),
+            Some(v) => {
+                if v == token.client_token {
+                    Ok(())
+                } else {
+                    Err(anyhow!("Client token does not match."))
+                }
+            }
         }
     }
 }
